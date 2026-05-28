01d3cc8f84209d504d9e9c891041af9e93b2377d
Arnuel Gutierrez Menco <argume31@hotmail.com>
2026-05-28 17:15:47 -0500

    redirect home to routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Welcome')->name('home');
+Route::redirect('/', 'routes')->name('home');
 Route::resource('routes', RouteController::class);
